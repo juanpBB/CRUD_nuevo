1ca3a28bf646d49222511858e4a4b65788bac192
Juan Pablo Blandon B <166535711+juanpBB@users.noreply.github.com>
2025-04-02 22:38:39 -0500

    definimos en nuestra app los documentos que seran usados en la base de datos de mongo en ese caso se guardara un documento con un nombre, autor y un titulo por cada ingreso del usuario

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ db= dbase.dbConection()
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/products', methods=['POST'])
+def addproduct():
+    products = db['products'] #
+    nombre = request.form['nombre'] 
+    titulo= request.form['titulo'] 
+    autor=request.form['autor'] 
+
+    if nombre and titulo and autor:
+        products = products(nombre,titulo,autor)
+        products.insert_one(products.paraConexiondb())
+        Response= jsonify({
+            'nombre': nombre,
+            'autor':autor,
+            'titulo':titulo
+        })
+        return redirect(url_for('home'))
+    else:
+        return notfound()
