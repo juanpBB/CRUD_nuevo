5f04bd641545f9397e790727ce26327ab23729ae
Juan Pablo Blandon B <166535711+juanpBB@users.noreply.github.com>
2025-04-02 22:35:18 -0500

    se agregado las rutas que manejara la aplicacion, manejara un archivo html y tambien se ha establecido la conexion con la base de datos en db=dbase.dbconection para nuestra base de datos mongodb

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from flask import Flask, render_template, request, Response, jsonify, redirect, url_for
 import database as dbase 
 from product import Producto  
+
+app= Flask(__name__)
+db= dbase.dbConection() 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
